0fcbc171c25587e6c66637847ae4df021755ea04
newsun-boki <1178303094@qq.com>
2023-03-08 18:23:51 +0800

    07

---
+++ flask_blog/0607_authentication_profile/flaskblog/forms.py
@@ -0,0 +1,53 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
+from flaskblog.models import User
+from flask_login import current_user
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    #FlaskForm会自动查找名叫validate_%name%的函数来进行验证
+    def validate_username(self,username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a new one')
+    
+    def validate_email(self,email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a new one')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+    #FlaskForm会自动查找名叫validate_%name%的函数来进行验证
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a new one')
+    
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a new one')
