ef100af90a97d40cc8fe664b1c95ae094919743e
newsun-boki <1178303094@qq.com>
2023-03-12 17:04:16 +0800

    change theme

---
+++ flask_blog/08_post_operations/flaskblog/models.py
@@ -0,0 +1,30 @@
+from flaskblog import db,login_manager
+from datetime import datetime
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    #这里的Post是大写是因为引用的是类名
+    posts = db.relationship('Post', backref='author', lazy=True)
+    #打印类的函数
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)#这里传入的utcnow没带括号说明传入的是函数本身
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
